9eb4e2530ee47ced5cb7df542e0e8253d49b3a32
MagdielCAS <magdielc.a.sousa@alu.ufc.br>
2017-03-26 11:44:40 -0300

    more intuitive spec

--- instagram_spec.rb
+++ instagram_spec.rb
@@ -3,12 +3,12 @@ require_relative 'instagram'
 describe Instagram, "#sentiment?" do
 
     context "when sentiment change" do
-        it "perform the request" do
+        it "has to change for positive, neutral or negative" do
             post = Instagram.new
-
+           
             post.perform_requisition("positive")
             expect(post.sentiment?).to eq "POSITIVE"
-
+    
             post.perform_requisition("neutral")
             expect(post.sentiment?).to eq "NEUTRAL"
 
@@ -16,5 +16,4 @@ describe Instagram, "#sentiment?" do
             expect(post.sentiment?).to eq "NEGATIVE"
         end
     end
-    
 end
