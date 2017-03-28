ed76ea8f7dda9e2869f2bfe06fc5c73144eb7444
MagdielCAS <magdielc.a.sousa@alu.ufc.br>
2017-03-28 16:44:06 -0300

    Added test for perform_requisition"

--- instagram_spec.rb
+++ instagram_spec.rb
@@ -1,27 +1,50 @@
 require_relative 'instagram'
 
-describe Instagram, "#sentiment?" do
+describe Instagram do
 
-    context "when sentiment change" do
-        it "should change to positive" do
-            post = Instagram.new
-           
-            post.perform_requisition("positive")
-            expect(post.sentiment?).to eq "POSITIVE"
+    describe ".perform_requisition" do
+
+        context "when change sentiment" do
+            it "has to succeed when positive" do
+                post = Instagram.new
+                expect(post.perform_requisition("positive")).to eq "200"
+            end
+
+            it "has to succeed when neutral" do
+                post = Instagram.new
+                expect(post.perform_requisition("neutral")).to eq "200"
+            end
+
+            it "has to succeed when negative" do
+                post = Instagram.new
+                expect(post.perform_requisition("negative")).to eq "200"
+            end
         end
+    end
 
-        it "should change to neutral" do
-            post = Instagram.new
-           
-            post.perform_requisition("neutral")
-            expect(post.sentiment?).to eq "NEUTRAL"
-        end
+    describe "#sentiment?" do
 
-        it "should change to negative" do
-            post = Instagram.new
-           
-            post.perform_requisition("negative")
-            expect(post.sentiment?).to eq "NEGATIVE"
+        context "when sentiment change" do
+            it "should change to positive" do
+                post = Instagram.new
+            
+                post.perform_requisition("positive")
+                expect(post.sentiment?).to eq "POSITIVE"
+            end
+
+            it "should change to neutral" do
+                post = Instagram.new
+            
+                post.perform_requisition("neutral")
+                expect(post.sentiment?).to eq "NEUTRAL"
+            end
+
+            it "should change to negative" do
+                post = Instagram.new
+            
+                post.perform_requisition("negative")
+                expect(post.sentiment?).to eq "NEGATIVE"
+            end
         end
     end
 end
